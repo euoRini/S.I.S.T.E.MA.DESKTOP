d68d39dd49f7155601462fc26fb7772df771a972
GustavoBazinga <63375206+GustavoBazinga@users.noreply.github.com>
2020-04-11 22:51:24 -0300

    Programa 100% Atualizado

--- S.I.S.T.E.MA/formMsgBox.Designer.vb
+++ S.I.S.T.E.MA/formMsgBox.Designer.vb
@@ -22,17 +22,29 @@ Partial Class formMsgBox
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(366, 21)
+        Me.Panel1.TabIndex = 0
         '
         'formMsgBox
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(32, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(366, 225)
+        Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "formMsgBox"
         Me.Text = "formMsgBox"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Panel1 As Panel
 End Class
